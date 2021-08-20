672c962a07dfe584532fdc964124d2aca940eca8
tim rotzinger <tim.rotzinger@gmail.com>
2021-08-20 20:26:45 +0000

    added some experimental cases

--- python_simple_compress.py
+++ python_simple_compress.py
@@ -3,9 +3,41 @@ so_file = "/home/trotzinger/garmin_question/simple_compress.so"
 
 simple_compress = CDLL(so_file)
 simple_compress.argtypes = [POINTER(c_ubyte), c_int]
-data_size = c_int(4)
-data_ptr = (c_ubyte * 4)(c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x55))
+#data_size = c_int(4)
+# test simple pos path no compression
+data_ptr1 = (c_ubyte * 4)(c_ubyte(0x22),c_ubyte(0x33),c_ubyte(0x44),c_ubyte(0x55))
+# test simple pos path with compression
+data_ptr2 = (c_ubyte * 4)(c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x22))
+# test overload max chunk size with compression (127)
+data_ptr3 = (c_ubyte * 128)(*[c_ubyte(0x22) for i in range(128)])
+# test empty 
+data_ptr4 = (c_ubyte * 0)()
+# test gap compressions
+data_ptr5 = (c_ubyte * 9)(c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x22),
+                          c_ubyte(0x66),
+                          c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x22))
+# test numbers too large no compression
+data_ptr6 = (c_ubyte * 4)(c_ubyte(0xAA),c_ubyte(0xBB),c_ubyte(0xCC),c_ubyte(0xDD))
+# test numbers too large no compression
+data_ptr7 = (c_ubyte * 4)(c_ubyte(0xAA),c_ubyte(0xAA),c_ubyte(0xAA),c_ubyte(0xAA))
 
-print(simple_compress.simple_compress(byref(data_ptr), data_ptr._length_))
-for d in data_ptr:
-    print(hex(d))
+#data_ptr3 = (c_ubyte * 4)(c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x22),c_ubyte(0x22))
+
+def results(data_ptr):
+    print(simple_compress.simple_compress(byref(data_ptr), data_ptr._length_))
+    for d in data_ptr:
+        print(hex(d))
+
+results(data_ptr1)
+print('-'*10)
+results(data_ptr2)
+print('-'*10)
+results(data_ptr3)
+print('-'*10)
+results(data_ptr4)
+print('-'*10)
+results(data_ptr5)
+print('-'*10)
+results(data_ptr6)
+print('-'*10)
+results(data_ptr7)
